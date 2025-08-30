f5d4fa0deb15634ffa68eb70b27b2568bb5836dc
holdn2 <yoocy01@gmail.com>
2025-08-31 01:17:50 +0900

    fix: 리뷰 반영 중 lint 에러 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,7 +3,7 @@ import { IcReact } from '../../assets/svg';
 const Home = () => {
   return (
     <div>
-      <img src={IcReact} alt='' />
+      <img src={IcReact} />
       <h1>Home Page</h1>
       <p>예시입니다!</p>
     </div>
