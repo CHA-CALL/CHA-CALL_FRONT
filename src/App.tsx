71336dde585a711b9594fda16e0b41022489d9f5
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-01 18:20:12 +0900

    init: tanstack query setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import '@styles/global.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@/router/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
