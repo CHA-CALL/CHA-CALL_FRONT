3d16489060b1253767d576f3d0c0da3a4981ec3b
glyph8 <newdy0808@gmail.com>
2025-09-01 15:28:46 +0900

    refactor: @types/node 제거 및 vite.config path 내용 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@api': path.resolve(__dirname, './src/api'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/shared/components'),
-      '@hooks': path.resolve(__dirname, './src/shared/hooks'),
-      '@utils': path.resolve(__dirname, './src/shared/utils'),
-      '@types': path.resolve(__dirname, './src/shared/types'),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 })
