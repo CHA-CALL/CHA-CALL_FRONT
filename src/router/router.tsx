3379d4d26e649773e8107d6fffa385b1df9a9ed7
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-04 13:36:36 +0900

    fix: lint error

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import Home from '@/pages/home/Home';
 import { ROUTES } from '@/router/constant/routes';
 import Layout from '@/router/Layout';
+
 export const router = createBrowserRouter([
   {
     element: <Layout />,
