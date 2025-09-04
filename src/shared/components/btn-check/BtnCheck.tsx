ed9bae5acf4f1871fba450055e0b7f50cca88bd6
glyph8 <newdy0808@gmail.com>
2025-09-05 01:57:51 +0900

    refactor: 중복 코드 제거

--- src/shared/components/btn-check/BtnCheck.tsx
+++ src/shared/components/btn-check/BtnCheck.tsx
@@ -7,24 +7,17 @@ export interface BtnClickProps {
 }
 
 export const BtnCheck = ({ isChecked, setIsChecked }: BtnClickProps) => {
+  const baseClasses =
+    'w-[1.8rem] h-[1.8rem] rounded-[0.6rem] flex justify-center items-center';
+  const checkedClasses = 'bg-primary-700';
+  const uncheckedClasses = 'bg-white outline-[-1] outline-grayscale-200';
   return (
-    <>
-      {isChecked ? (
-        <button
-          type='button'
-          className='w-[1.8rem] h-[1.8rem] flex justify-center items-center bg-primary-700 rounded-[0.6rem]'
-          onClick={() => setIsChecked(false)}
-        >
-          <Icon name='ic_check_2' width={8} height={7}/>
-        </button>
-      ) : (
-        <button
-          type='button'
-          className='w-[1.8rem] h-[1.8rem] bg-white outline-[-1] outline-grayscale-200 rounded-[0.6rem]'
-          onClick={() => setIsChecked(true)}
-        >
-        </button>
-      )}
-    </>
+    <button
+      type='button'
+      className={`${baseClasses} ${isChecked ? checkedClasses : uncheckedClasses}`}
+      onClick={() => setIsChecked(!isChecked)} // 핸들러 로직도 통합
+    >
+      {isChecked && <Icon name='ic_check_2' width={8} height={7} />}
+    </button>
   );
 };
