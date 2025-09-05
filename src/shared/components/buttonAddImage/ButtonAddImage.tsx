1d9619de2e26e4383d46a4f3930667995f1a1543
junghyungo <pp90971658@gmail.com>
2025-09-05 16:40:59 +0900

    feat: 카메라 아이콘 svg 수정 및 서류 첨부 버튼 컴포넌트 구현

---
+++ src/shared/components/buttonAddImage/ButtonAddImage.tsx
@@ -0,0 +1,59 @@
+import { useRef } from 'react';
+import { Icon } from '@components/icon/Icon';
+import { cn } from '@utils/cn';
+
+interface ButtonAddImageProps {
+  onFileSelect?: (file: File) => void
+  multiple?: boolean
+}
+
+export default function ButtonAddImage({
+  onFileSelect,
+  multiple = false,
+}: ButtonAddImageProps) {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      if (multiple) {
+        Array.from(files).forEach(file => onFileSelect?.(file));
+      } else {
+        onFileSelect?.(files[0]);
+      }
+    }
+    event.target.value = '';
+  };
+
+  return (
+    <>
+      <input
+        ref={fileInputRef}
+        type='file'
+        accept='image/*'
+        multiple={multiple}
+        onChange={handleFileChange}
+        className='hidden'
+      />
+      <button
+        type='button'
+        onClick={handleButtonClick}
+        className={cn(
+          'flex flex-col items-center',
+          'pl-[2.1rem] pr-[2rem] pt-[2.1rem] pb-[1.8rem] rounded-[1.6rem]',
+          'bg-white border border-grayscale-200',
+          'hover:bg-grayscale-50 duration-200',
+        )}
+      >
+        <Icon name='ic_camera' />
+        <span className='mt-[0.6rem] text-grayscale-300 text-[1rem]'>
+          서류 첨부
+        </span>
+      </button>
+    </>
+  )
+}
