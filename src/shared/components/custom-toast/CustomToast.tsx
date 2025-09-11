f6716c40f246155e81ad27065e4b72ea47c57c78
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-11 17:29:03 +0900

    fix: lint error

--- src/shared/components/custom-toast/CustomToast.tsx
+++ src/shared/components/custom-toast/CustomToast.tsx
@@ -1,18 +1,18 @@
-import { type ToastContentProps } from "react-toastify"
-
+import React from 'react';
+import { type ToastContentProps } from 'react-toastify';
 
 interface CustomToastProps extends ToastContentProps {
-    text?: string,
-    icon?: React.ReactNode,
-    children?: React.ReactNode,
+  text?: string;
+  icon?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
-export default function CustomToast({text, icon, children}: CustomToastProps) {
+export default function CustomToast({ text, icon, children }: CustomToastProps) {
   return (
-    <div  className="flex items-center gap-[1rem] px-[1.6rem] w-[33.5rem] h-[4rem] py-[0.9rem] bg-grayscale-900 text-white rounded-[1.6rem]">
-        {icon && icon}
-        {text && <p className="caption-m-12">{text}</p>}
-        {children && children}
+    <div className="flex items-center gap-[1rem] px-[1.6rem] w-[33.5rem] h-[4rem] py-[0.9rem] bg-grayscale-900 text-white rounded-[1.6rem]">
+      {icon && icon}
+      {text && <p className="caption-m-12">{text}</p>}
+      {children && children}
     </div>
-  )
+  );
 }
