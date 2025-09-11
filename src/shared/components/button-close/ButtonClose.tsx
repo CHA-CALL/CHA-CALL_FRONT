e82112b79706fa620dbfa62d9a4cb4eb5697f17e
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-11 13:16:12 +0900

    feat: headwind setting

--- src/shared/components/button-close/ButtonClose.tsx
+++ src/shared/components/button-close/ButtonClose.tsx
@@ -8,22 +8,18 @@ interface ButtonCloseProps {
 
 export default function ButtonClose({
   handleClick,
-  className
+  className,
 }: ButtonCloseProps) {
   return (
     <button
       type='button'
       onClick={handleClick}
       className={cn(
-        'flex px-[0.64rem] py-[0.64rem] bg-grayscale-900 rounded-full',
+        'bg-grayscale-900 flex rounded-full px-[0.64rem] py-[0.64rem]',
         className
       )}
     >
-      <Icon
-        name='ic_close_white'
-        width={12}
-        height={12}
-      />
+      <Icon name='ic_close_white' width={12} height={12} />
     </button>
-  )
+  );
 }
