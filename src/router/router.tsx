f1f8ed8ae2f80488854e18e9f5919ba630d2b6c8
junghyungo <pp90971658@gmail.com>
2025-09-13 13:16:56 +0900

    feat: 예약하기 페이지 routes 설정

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Home from '@/pages/home/Home';
+import Reservation from '@/pages/reservation/Reservation';
 import { ROUTES } from '@/router/constant/routes';
 import Layout from '@/router/Layout';
 
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
       {
         path: ROUTES.HOME,
         element: <Home />,
+      },
+      {
+        path: ROUTES.RESERVATION,
+        element: <Reservation />,
       },
     ],
   },
