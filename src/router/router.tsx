4fc42a2e134432c5944180e0fd271c60653797bf
junghyungo <pp90971658@gmail.com>
2025-09-14 14:05:46 +0900

    docs: routes 수정

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import Home from '@/pages/home/Home';
-import Reservation from '@/pages/reservation/Reservation';
 import { ROUTES } from '@/router/constant/routes';
 import Layout from '@/router/Layout';
+import Home from '@/pages/home/Home';
+import Filter from '@pages/filter/Filter';
+import Reservation from '@/pages/reservation/Reservation';
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: ROUTES.HOME,
         element: <Home />,
+      },
+      {
+        path: ROUTES.FILTER,
+        element: <Filter />,
       },
       {
         path: ROUTES.RESERVATION,
