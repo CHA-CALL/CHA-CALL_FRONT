80312eb41750c45c866ffd6961b4c7bfedcceecb
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-16 21:35:43 +0900

    feat: modal 삭제 후 overlay 생성

--- src/shared/components/bottom-sheet/BottomSheet.tsx
+++ src/shared/components/bottom-sheet/BottomSheet.tsx
@@ -37,14 +37,7 @@ export default function BottomSheet({
     sheetHeight,
   });
   return (
-    <div
-      className={cn(
-        'fixed inset-[0] h-dvh w-dvw bg-black/50',
-        'z-50 transition-opacity duration-300 ease-in-out',
-        isOpen ? 'opacity-100' : 'pointer-events-none opacity-0'
-      )}
-      onClick={handleCloseBottomSheet}
-    >
+   
       <div
         ref={sheetRef}
         className={cn(
@@ -57,6 +50,6 @@ export default function BottomSheet({
         <div className='mx-auto my-[1rem] h-[0.35rem] w-[4.1rem] rounded-[10rem] bg-grayscale-300' />
         <div className='mb-[3.4rem] mt-[2.8rem]'>{sheetContent}</div>
       </div>
-    </div>
+   
   );
 }
---
+++ src/shared/components/overlay/Overlay.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef } from "react";
+import { cn } from "@shared/utils/cn";
+
+interface OverlayProps {
+  isOpen?: boolean;
+  position?: "center" | "bottom" | "top" | "left" | "right";
+  handleClose: () => void;
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export default function Overlay({
+  isOpen = false,
+  position = "center",
+  handleClose,
+  children,
+  className,
+  ...props
+}: OverlayProps) {
+  const prevOverflowRef = useRef<string>("");
+
+  useEffect(() => {
+    if (isOpen) {
+      prevOverflowRef.current = document.body.style.overflow;
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = prevOverflowRef.current || "";
+    }
+    return () => {
+      document.body.style.overflow = prevOverflowRef.current || "";
+    };
+  }, [isOpen]);
+
+  const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target === e.currentTarget) {
+      handleClose();
+    }
+  };
+
+  const getPositionClasses = () => {
+    switch (position) {
+      case "bottom":
+        return "items-end justify-center";
+      case "top":
+        return "items-start justify-center";
+      case "left":
+        return "items-center justify-start";
+      case "right":
+        return "items-center justify-end";
+      default:
+        return "items-center justify-center";
+    }
+  };
+
+
+
+  return (
+    <div
+      onClick={handleOverlayClick}
+      className={cn(
+        "fixed inset-0 flex max-w-[60rem] w-full h-dvh bg-black/50 z-100",
+        getPositionClasses(),
+        "transition-opacity duration-300 ease-in-out",
+        isOpen ? "opacity-100" : "pointer-events-none opacity-0",
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
