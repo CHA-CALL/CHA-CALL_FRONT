04dea5cc456b84bd5a8ada26b01c66aeb218f20c
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-17 17:33:11 +0900

    feat: zustand 설치 및 역할 관련 hook/ constant 생성

---
+++ src/shared/hooks/useRole.ts
@@ -0,0 +1,16 @@
+import { type Role, ROLE } from '@shared/constant/role';
+import { create } from 'zustand';
+
+interface RoleState {
+  role: Role;
+  setRole: (role: Role) => void;
+  getRole: () => Role;
+  updateRole: (role: Role) => void;
+}
+
+export const useRole = create<RoleState>((set, get) => ({
+  role: ROLE.USER,
+  setRole: (role: Role) => set({ role }),
+  getRole: () => get().role,
+  updateRole: (role: Role) => set({ role }),
+}));
