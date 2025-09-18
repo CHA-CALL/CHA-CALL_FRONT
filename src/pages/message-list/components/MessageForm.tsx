759c46c386e1f53d0d1d489cb8f19f72e5f4e614
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-18 16:41:06 +0900

    feat: message form modal 구현

--- src/pages/message-list/components/MessageForm.tsx
+++ src/pages/message-list/components/MessageForm.tsx
@@ -1,42 +1,81 @@
 import Button from '@shared/components/button/Button';
 import { Icon } from '@shared/components/icon/Icon';
 import Navigation from '@shared/components/navigation/Navigation';
+import Overlay from '@shared/components/overlay/Overlay';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function MessageForm() {
   const navigate = useNavigate();
   const [message, setMessage] = useState('');
-  const [hasChanges, setHasChanges] = useState(false);
+  const [submit, setSubmit] = useState(false);
   const MAX_LENGTH = 100;
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleChangeMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(e.target.value);
-    setHasChanges(true);
+    setSubmit(false);
   };
 
   const handleClickSave = () => {
     //TODO: API 연동
-    setHasChanges(false);
+    setSubmit(true);
     //TODO: Toast 알림 표시
     navigate('/message-list');
   };
 
   const handleClickBack = () => {
-    if (hasChanges && message.trim()) {
-      const confirmExit = window.confirm('저장하지 않고 나가시겠습니까?');
-      if (confirmExit) {
-        navigate('/message-list');
-      }
+    if (!submit && message.trim()) {
+      setIsOpen(true);
     } else {
       navigate('/message-list');
     }
   };
 
   const isDisabled = message.length === 0;
+  const handleClickCancel = () => {
+    setIsOpen(false);
+  };
+
+  const handleClickConfirm = () => {
+    setIsOpen(false);
+    navigate('/message-list');
+  };
 
   return (
     <div>
+      <Overlay isOpen={isOpen} handleClose={() => setIsOpen(false)}>
+        <div className='flex w-[27.4rem] flex-col items-center justify-center gap-[1.6rem] rounded-[1.6rem] bg-white px-[2rem] pb-[2rem] pt-[2.4rem]'>
+          <div className='flex w-full flex-col justify-start gap-[0.2rem]'>
+            <p className='title-sb-16 text-grayscale-900'>
+              저장하지 않고 나가시겠습니까?
+            </p>
+            <p className='caption-m-12 text-grayscale-700'>
+              작성 중인 내용은 저장되지 않으며,
+              <br />
+              나가면 모두 삭제됩니다.
+            </p>
+          </div>
+          <div className='flex justify-between gap-[1rem]'>
+            <Button
+              variant='cta'
+              buttonStyle='sub'
+              className='w-[11.2rem]'
+              handleClickButton={handleClickConfirm}
+            >
+              나가기
+            </Button>
+            <Button
+              variant='cta'
+              buttonStyle='active'
+              className='w-[11.2rem]'
+              handleClickButton={handleClickCancel}
+            >
+              취소
+            </Button>
+          </div>
+        </div>
+      </Overlay>
       <Navigation
         text='자주 쓰는 메세지 설정'
         leftIcon={<Icon name='ic_back' />}
