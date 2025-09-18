3c039202ddfb5ddcd7e0208a60a9c05fb595db8b
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-18 10:39:58 +0900

    fix: 컴포넌트 변경

--- src/pages/chat-list/ChatList.tsx
+++ src/pages/chat-list/ChatList.tsx
@@ -2,9 +2,10 @@ import ChatListManageBar from '@pages/chat-list/components/ChatListManageBar';
 import { useChatList } from '@pages/chat-list/hooks/use-chat-list';
 import ChatListItem from '@shared/components/chat-list-item/ChatListItem';
 import { Icon } from '@shared/components/icon/Icon';
-import Modal from '@shared/components/modal/Modal';
+import OverlayModal from '@shared/components/overlay/Overlay';
 import Navigation from '@shared/components/navigation/Navigation';
 import { useNavigate } from 'react-router-dom';
+import Button from '@shared/components/button/Button';
 
 export default function ChatList() {
   const navigate = useNavigate();
@@ -40,16 +41,35 @@ export default function ChatList() {
         handleSelectOff={handleSelectOff}
         handleDeleteChat={handleDeleteChat}
       />
-      <Modal
+      <OverlayModal
         isOpen={isDeleteModalOpen}
-        title={'선택한 대화를 삭제할까요?'}
-        content={`${selectChatList.size}건이 삭제되며 되돌릴 수 없습니다.`}
-        cancelChildren={'취소'}
-        confirmChildren={'삭제'}
-        handleCancelClick={handleCloseModal}
-        handleConfirmClick={handleCloseModal}
-        handleModalClose={handleCloseModal}
-      />
+        position='center'
+        handleClose={handleCloseModal}
+      >
+        <div className='flex min-w-[27.4rem] flex-col gap-[1.6rem] rounded-[1.6rem] bg-white px-[2rem] pb-[2rem] pt-[2.4rem]'>
+          <div>
+            <h3 className='title-sb-16'>선택한 대화를 삭제할까요?</h3>
+            <p className='caption-m-12 text-grayscale-700'>{`${selectChatList.size}건이 삭제되며 되돌릴 수 없습니다.`}</p>
+          </div>
+          <div className='flex gap-[1rem]'>
+            <Button
+              variant='cta'
+              buttonStyle='sub'
+              handleClickButton={handleCloseModal}
+            >
+              취소
+            </Button>
+            <Button
+              variant='cta'
+              buttonStyle='active'
+              handleClickButton={handleCloseModal}
+            >
+              삭제
+            </Button>
+          </div>
+        </div>
+      </OverlayModal>
+
       <div className='scrollbar-hide flex flex-1 flex-col overflow-y-scroll py-[1.2rem]'>
         {(chatList ?? []).map(item => {
           return (
