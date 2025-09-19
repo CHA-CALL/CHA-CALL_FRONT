d8a3f48c885f01d3fb9507264f090a496979c851
junghyungo <pp90971658@gmail.com>
2025-09-19 11:44:32 +0900

    feat: 기존 userType 삭제

--- src/shared/types/user-types.ts
+++
@@ -1,8 +0,0 @@
-export const USER_TYPE = {
-  Guest: 'guest',
-  Customer: 'customer',
-  Manager: 'manager',
-} as const;
-
-export type UserTypeKey = keyof typeof USER_TYPE;
-export type UserType = (typeof USER_TYPE)[UserTypeKey];
