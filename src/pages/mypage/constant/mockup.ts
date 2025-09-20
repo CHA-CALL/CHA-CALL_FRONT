9902242d877c7059a2c3314ba5d151e2905b86cb
holdn2 <yoocy01@gmail.com>
2025-09-20 21:45:24 +0900

    feature: 마이페이지 퍼블리싱 완료

---
+++ src/pages/mypage/constant/mockup.ts
@@ -0,0 +1,5 @@
+export const user_mockup = {
+  name: '테스트',
+  email: 'test@kakao.com',
+  gender: 'male',
+};
--- src/router/constant/routes.ts
+++ src/router/constant/routes.ts
@@ -5,6 +5,8 @@ export const ROUTES = {
   FOOD_TRUCK_ONBOARDING: '/owner/food-truck-onboarding',
   SET_LOCATION: '/set-location',
   CHATLIST: '/chat-list',
-  MESSAGELIST: '/owner/message-list',
+  MESSAGE_LIST: '/owner/message-list',
   MESSAGE_FORM: '/owner/message-form',
+  MYPAGE: '/mypage',
+  PROFILE_SETTING: '/profile-setting',
 };
