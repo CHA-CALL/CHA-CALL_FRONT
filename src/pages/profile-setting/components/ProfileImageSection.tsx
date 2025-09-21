43bd4a9c0c41edb7380bedaa202b428e7bf62284
holdn2 <yoocy01@gmail.com>
2025-09-21 14:08:05 +0900

    code review: 코드리뷰 반영

--- src/pages/profile-setting/components/ProfileImageSection.tsx
+++ src/pages/profile-setting/components/ProfileImageSection.tsx
@@ -12,7 +12,11 @@ export default function ProfileImageSection({
 }: ProfileImageSectionProps) {
   const profileImage = profileImageUrl ? profileImageUrl : DefaultProfile;
   return (
-    <div className='relative p-[0.5rem]' onClick={handleOpenBottomSheet}>
+    <button
+      type='button'
+      className='relative p-[0.5rem]'
+      onClick={handleOpenBottomSheet}
+    >
       <img
         className='h-[8rem] w-[8rem] rounded-full'
         src={profileImage}
@@ -21,6 +25,6 @@ export default function ProfileImageSection({
       <div className='absolute bottom-[0.8rem] right-[0] z-50 flex h-[3rem] w-[3rem] items-center rounded-[1.5rem] border border-grayscale-200 bg-white p-[0.5rem]'>
         <Icon name='ic_camera' color='#838992' />
       </div>
-    </div>
+    </button>
   );
 }
