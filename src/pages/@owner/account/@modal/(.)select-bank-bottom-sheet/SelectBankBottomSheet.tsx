0b898cd841eb0ecce9761a50314a389b0e4b9d28
holdn2 <yoocy01@gmail.com>
2025-09-23 10:35:30 +0900

    fix: 바텀시트 변경에 따른 기존 바텀시트 사용 수정

--- src/pages/@owner/account/@modal/(.)select-bank-bottom-sheet/SelectBankBottomSheet.tsx
+++ src/pages/@owner/account/@modal/(.)select-bank-bottom-sheet/SelectBankBottomSheet.tsx
@@ -20,14 +20,14 @@ export default function SelectBankBottomSheet({
       isOpen={isOpen}
       handleCloseBottomSheet={handleClose}
       sheetContent={
-        <div className='flex flex-col gap-[2rem]'>
-          <p className='heading-sb-18 text-black'>은행선택</p>
+        <div className='mt-[2rem] flex flex-col gap-[2rem] px-[0.5rem]'>
+          <p className='text-black heading-sb-18'>은행선택</p>
           <div className='flex w-full flex-col'>
             {Object.values(BANK).map(option => (
               <button
                 key={option}
                 onClick={() => handleChange(option)}
-                className='body-m-14 text-grayscale-700 flex w-full items-center justify-between py-[1.5rem]'
+                className='flex w-full items-center justify-between py-[1.5rem] text-grayscale-700 body-m-14'
               >
                 <p
                   className={cn(
