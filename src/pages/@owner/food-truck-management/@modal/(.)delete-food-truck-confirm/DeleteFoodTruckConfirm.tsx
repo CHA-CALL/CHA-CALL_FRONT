5d8bd4c49c2d0938dd9ca21af1c926620a8482e8
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-09-26 09:48:28 +0900

    feat: 삭제 확인 모달 구현

---
+++ src/pages/@owner/food-truck-management/@modal/(.)delete-food-truck-confirm/DeleteFoodTruckConfirm.tsx
@@ -0,0 +1,45 @@
+import Overlay from '@shared/components/overlay/Overlay';
+import Button from '@shared/components/button/Button';
+
+interface DeleteFoodTruckConfirmProps {
+  isOpen: boolean;
+  handleClose: () => void;
+  handleClickConfirm: () => void;
+}
+
+export default function DeleteFoodTruckConfirm({
+  isOpen,
+  handleClose,
+  handleClickConfirm,
+}: DeleteFoodTruckConfirmProps) {
+  return (
+    <Overlay isOpen={isOpen} handleClose={handleClose}>
+      <div className='flex min-w-[27.4rem] flex-col gap-[1.6rem] rounded-[1.6rem] bg-white px-[2rem] pb-[2rem] pt-[2.4rem]'>
+        <div className='flex w-full flex-col justify-start gap-[0.2rem]'>
+          <p className='title-sb-16 text-grayscale-900'>
+            이 푸드트럭을 삭제할까요?
+          </p>
+          <p className='caption-m-12 text-grayscale-700'>
+            삭제 후에는 되돌릴 수 없습니다.
+          </p>
+        </div>
+        <div className='flex gap-[1rem]'>
+          <Button
+            variant='cta'
+            buttonStyle='sub'
+            handleClickButton={handleClose}
+          >
+            취소
+          </Button>
+          <Button
+            variant='cta'
+            buttonStyle='active'
+            handleClickButton={handleClickConfirm}
+          >
+            삭제
+          </Button>
+        </div>
+      </div>
+    </Overlay>
+  );
+}
