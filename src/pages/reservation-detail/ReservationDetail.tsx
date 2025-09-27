0b4a5e6662516f08b833e38f8141b808966046a4
glyph8 <newdy0808@gmail.com>
2025-09-28 00:27:46 +0900

    feat: 렌더링할 데이터 형식 object에서전처리 후  Array로 변경

--- src/pages/reservation-detail/ReservationDetail.tsx
+++ src/pages/reservation-detail/ReservationDetail.tsx
@@ -11,10 +11,13 @@ import { INITIAL_DATA, MOCKUP_DATA } from './constant/reservation-detail';
 import Button from '@shared/components/button/Button';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '@router/constant/routes';
+import Tooltip from '@shared/components/tooltip/Tooltip';
 
 export default function ReservationDetail() {
   const { role } = useRole();
   const isProvider = role === ROLE.PROVIDER;
+  // TODO : 추후 툴팁 관련 커스텀 훅 만들어 관리
+  const [isOpenTip, setIsOpenTip] = useState(true);
   const [reservationData, setReservationData] =
     useState<ReservationResponse>(INITIAL_DATA);
   const {
@@ -29,27 +32,50 @@ export default function ReservationDetail() {
   } = reservationData;
   const navigate = useNavigate();
 
-  const reservationInfo = {
-    장소: `${address} ${detailAddress}`,
-    날짜: reservationDates,
-    시간: operationHour,
-  };
+  // 순수 텍스트, 날짜 - 날짜별 줄바꿈, 금액 구분자 추가하기, boolean 따라 가능/불가능.
+  const reservationInfo = [
+    {
+      label: '장소',
+      data: `${address} ${detailAddress}`,
+    },
+    {
+      label: '날짜',
+      data: reservationDates?.join('\n'),
+    },
+    {
+      label: '시간',
+      data: operationHour,
+    },
+  ];
 
-  const operationInfo = {
-    음식: menu,
-    금액: deposit,
-  };
+  const operationInfo = [
+    {
+      label: '음식',
+      data: menu,
+    },
+    {
+      label: '결제금',
+      data: `${deposit?.toLocaleString('ko-kr')} 원`,
+    },
+  ];
 
-  const etcInfo = {
-    '전기 사용 유무': isUseElectricity,
-    '기타 요청 사항': etcRequest,
-  };
+  const etcInfo = [
+    {
+      label: '전기 사용 유무',
+      data: isUseElectricity ? '가능' : '불가능',
+    },
+    {
+      label: '기타 요청 사항',
+      data: etcRequest,
+    },
+  ];
 
   const handleClickBack = () => {
     navigate(ROUTES.RESERVATION);
   };
   const handleDownload = () => {
     //TODO : 다운로드 API 연동 예정
+    alert('예약 페이지 pdf 다운로드');
   };
   useEffect(() => {
     setReservationData(MOCKUP_DATA);
@@ -62,31 +88,44 @@ export default function ReservationDetail() {
         leftIcon={<Icon name='ic_back' />}
         handleLeftClick={handleClickBack}
         rightIcon={
-          <Button
-            children={
-              <Icon
-                name='ic_download'
-                className='text-grayscale-500 h-[1.2rem] w-[1.1rem]'
-              />
-            }
-            variant={'default'}
-            buttonStyle={'sub'}
-            className='flex h-[2.8rem] w-[2.8rem] items-center justify-center p-[0.5rem]'
-          />
+          <>
+            <Button
+              onClick={handleDownload}
+              children={
+                <>
+                  <Icon
+                    name='ic_download'
+                    className='text-grayscale-500 h-[1.2rem] w-[1.1rem]'
+                  />
+                </>
+              }
+              variant={'default'}
+              buttonStyle={'sub'}
+              className='flex h-[2.8rem] w-[2.8rem] items-center justify-center p-[0.5rem]'
+            />
+            <Tooltip
+              text={'PDF로 다운받아 보세요!'}
+              isTooltipVisible={isOpenTip}
+              handleCloseTooltip={() => setIsOpenTip(false)}
+              positionOffsetY={3.8}
+              positionOffsetX={1.4}
+              horizontalAlign='right'
+              verticalAlign='bottom'
+            />
+          </>
         }
-        handleRightClick={handleDownload}
       />
+
       <div className='flex h-[calc(100vh-4.8rem)] flex-col pb-[1.6rem]'>
-        {/* {isProvider ? (
+        {isProvider ? (
           <ClientInfoHeader foodTruckName={''} clientName={''} />
         ) : (
           <ProviderInfoHeader foodTruckName={'오소리 푸드트럭'} />
-        )} */}
+        )}
         <ClientInfoHeader
           foodTruckName={'오소리 푸드트럭'}
           clientName={'이현준'}
         />
-        <Button children={123} variant={'default'} buttonStyle={'sub'} />
         <div className='p-[2rem]'>
           <InfoCard title={'예약 내역'} infoList={reservationInfo} />
           <div className='border-grayscale-100 my-[2.4rem] border' />
--- src/pages/reservation-detail/components/InfoCard.tsx
+++ src/pages/reservation-detail/components/InfoCard.tsx
@@ -1,37 +1,25 @@
 import type { ReservationResponse } from 'apis/data-contracts';
 
+interface ReservationPartialInfo {
+  label: string;
+  data: string | undefined;
+}
+
 interface InfoCardProps {
   title: string;
-  infoList: ReservationResponse;
+  infoList: ReservationPartialInfo[];
 }
 
 export default function InfoCard({ title, infoList }: InfoCardProps) {
   return (
-    <div className='flex flex-col gap-[1.6rem] p-[0.5rem]'>
+    <div className='title-sb-12 flex flex-col gap-[1.6rem] p-[0.5rem]'>
       <h2 className='title-sb-14 text-grayscale-900'>{title}</h2>
-      {Object.entries(infoList).map(([label, data]) => (
-        <div className='title-sb-12 flex flex-col gap-[1.6rem]'>
-          {Array.isArray(data) ? (
-            <p className='flex justify-between'>
-              <span className='text-grayscale-500 whitespace-nowrap'>
-                {label}
-              </span>
-              <div className='flex flex-col'>
-                {data.map(item => (
-                  <span className='text-grayscale-700 text-end'>{item}</span>
-                ))}
-              </div>
-            </p>
-          ) : (
-            <p className='flex justify-between'>
-              <span className='text-grayscale-500 whitespace-nowrap'>
-                {label}
-              </span>
-              <span className='text-grayscale-700 text-end'>
-                {typeof data === 'boolean' ? (data ? '가능' : '불가능') : data}
-              </span>
-            </p>
-          )}
+      {infoList.map(({ label, data }) => (
+        <div key={label} className='flex justify-between'>
+          <span className='text-grayscale-500 whitespace-nowrap'>{label}</span>
+          <span className='text-grayscale-700 w-[40ch] whitespace-pre-line text-balance text-end'>
+            {data}
+          </span>
         </div>
       ))}
     </div>
