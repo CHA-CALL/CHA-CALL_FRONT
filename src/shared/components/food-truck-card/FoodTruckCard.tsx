840443a3e77a5aed4bd3329465780f115e9cc68c
junghyungo <pp90971658@gmail.com>
2025-09-30 00:06:08 +0900

    code review: 유저 예약내역 카드 수정

--- src/shared/components/food-truck-card/FoodTruckCard.tsx
+++ src/shared/components/food-truck-card/FoodTruckCard.tsx
@@ -53,20 +53,22 @@ export default function FoodTruckCard(props: FoodTruckCardProps) {
         );
       }
 
-      case FOOD_TRUCK_CARD_VARIANTS.RESERVATION_CLIENT:
+      case FOOD_TRUCK_CARD_VARIANTS.RESERVATION_CLIENT: {
+        const { period, time } = formatDateTimeInfos(props.data.dateTimeInfos);
+
         return (
           <>
             <img
-              src={props.image}
-              alt={props.foodTruckName}
+              src={props.data.photoUrl}
+              alt={props.data.name}
               className='w-[8rem] h-[8rem] mr-[1.8rem] rounded-[1.6rem] object-cover'
             />
 
             <div className='flex flex-col gap-[0.2rem]'>
-              <span className='title-sb-16 text-grayscale-900'>{props.foodTruckName}</span>
-              <InfoRow iconId='ic_locate'>{props.location}</InfoRow>
-              <InfoRow iconId='ic_calendar'>{props.period}</InfoRow>
-              <InfoRow iconId='ic_time'>{props.time}</InfoRow>
+              <span className='title-sb-16 text-grayscale-900'>{props.data.name}</span>
+              <InfoRow iconId='ic_locate'>{props.data.address || ''}</InfoRow>
+              <InfoRow iconId='ic_calendar'>{period}</InfoRow>
+              <InfoRow iconId='ic_time'>{time}</InfoRow>
             </div>
 
             <button
@@ -78,6 +80,7 @@ export default function FoodTruckCard(props: FoodTruckCardProps) {
             </button>
           </>
         );
+      }
 
       case FOOD_TRUCK_CARD_VARIANTS.FOODTRUCK_PROVIDER:
         return (
