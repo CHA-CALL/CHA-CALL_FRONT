ba210a55a84c45a895c0d0e59dfef5264830a3f1
junghyungo <pp90971658@gmail.com>
2025-09-30 13:56:56 +0900

    code review: MenuPriceInput input 수정

--- src/pages/@owner/menu/components/MenuPriceInput.tsx
+++ src/pages/@owner/menu/components/MenuPriceInput.tsx
@@ -14,25 +14,28 @@ export default function MenuPriceInput({
   error,
   onChange,
 }: MenuPriceInputProps) {
-  const handleChangePrice = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    onChange(e.target.value);
+  const handleChangePrice = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value.replace(/[^0-9]/g, ''));
   };
 
   return (
     <>
       <span className='ml-[0.5rem] mb-[1rem] title-sb-14 text-grayscale-900'>가격</span>
-      <textarea
-        className='
-          h-[5.6rem] mb-[2rem] px-[2rem] py-[1.65rem] rounded-[1.6rem]
-          body-m-14 text-grayscale-900 border-grayscale-300 caret-primary-700
-          placeholder:body-m-14 placeholder:text-grayscale-300
-          focus:border-grayscale-500 focus:outline-none
-        '
-        placeholder='텍스트를 입력해주세요.'
-        value={value}
-        onChange={handleChangePrice}
-        maxLength={MENU_TEXT.PRICE_MAX_LENGTH}
-      />
+      <div className='mb-[2rem] rounded-[1.6rem] border border-grayscale-300 focus-within:border-grayscale-500'>
+        <input
+          type='text'
+          inputMode='numeric'
+          className='
+            px-[2rem] py-[1.65rem] rounded-[1.6rem]
+            body-m-14 text-grayscale-900 caret-primary-700
+            placeholder:body-m-14 placeholder:text-grayscale-300
+          '
+          placeholder='텍스트를 입력해주세요.'
+          value={value}
+          onChange={handleChangePrice}
+          maxLength={MENU_TEXT.PRICE_MAX_LENGTH}
+        />
+      </div>
       {error && (
         <div className='mt-[-1.8rem]'>
           <ErrorText text={error.message || ''} />
