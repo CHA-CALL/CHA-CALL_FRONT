bb2539bfd6396ed95860f097d819c7d85f6f50f2
junghyungo <pp90971658@gmail.com>
2025-10-02 17:47:07 +0900

    code review: CardImage 컴포넌트

--- src/shared/components/food-truck-card/components/CardImage.tsx
+++ src/shared/components/food-truck-card/components/CardImage.tsx
@@ -1,5 +1,24 @@
-export default function CardImage() {
+import { cn } from '@utils/cn';
+
+interface CardImageProps {
+  imageUrl: string;
+  altText?: string;
+  className?: string;
+}
+
+export default function CardImage({
+  imageUrl,
+  altText,
+  className
+}: CardImageProps) {
   return (
-    <></>
+    <img
+      src={imageUrl}
+      alt={altText}
+      className={cn(
+        'rounded-[1.6rem] object-cover',
+        className,
+      )}
+    />
   );
 }
