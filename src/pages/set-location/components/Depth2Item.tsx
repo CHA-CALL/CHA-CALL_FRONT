b4b566fda9937929fa273e3bb229778aaf5fca66
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-04 20:57:59 +0900

    refacor: 파일명 변경

--- src/pages/set-location/components/Depth2Item.tsx
+++ src/pages/set-location/components/Depth2Item.tsx
@@ -1,22 +1,22 @@
 import { cn } from '@shared/utils/cn';
 
-interface SiGunGuItemProps {
+interface Depth2ItemProps {
   title: string;
-  isSelected: boolean;
-  handleSelectSiGunGu: () => void;
+  isSelected?: boolean;
+  handleSelectDepth2: () => void;
 }
 const BaseClass =
   'w-full h-[4.2rem] px-[2rem] py-[0.7rem] bg-white body-m-14 text-grayscale-700';
 
-export default function SiGunGuItem({
+export default function Depth2Item({
   title,
-  isSelected,
-  handleSelectSiGunGu,
-}: SiGunGuItemProps) {
+  isSelected = false,
+  handleSelectDepth2,
+}: Depth2ItemProps) {
   return (
     <button
       type='button'
-      onClick={handleSelectSiGunGu}
+      onClick={handleSelectDepth2}
       className={cn(
         BaseClass,
         isSelected && 'bg-primary-50 title-sb-14 text-primary-700'
--- src/pages/set-location/constant/set-location.ts
+++
@@ -1,2 +0,0 @@
-export const MAX_SELECTED = 10;
-export const SELECT_ALL_ID_LENGTH = 5;
