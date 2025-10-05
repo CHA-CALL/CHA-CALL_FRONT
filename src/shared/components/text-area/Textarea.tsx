15070610d32b3cd297690ff6ac025d10ae235836
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-05 10:15:32 +0900

    feat: textArea 공컴 생성

---
+++ src/shared/components/text-area/Textarea.tsx
@@ -0,0 +1,33 @@
+import type { ChangeEvent, TextareaHTMLAttributes } from 'react';
+import { cn } from '@utils/cn';
+
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  value: string;
+  placeholder?: string;
+  handleChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+  maxLength?: number;
+  className?: string;
+}
+
+export default function Textarea({
+  value,
+  placeholder = '텍스트를 입력해주세요.',
+  handleChange,
+  maxLength,
+  className,
+  ...props
+}: TextareaProps) {
+  return (
+    <textarea
+      className={cn(
+        'body-m-14 text-grayscale-900 border-grayscale-300 caret-primary-700 placeholder:body-m-14 placeholder:text-grayscale-300 focus:border-grayscale-500 min-h-[34rem] rounded-[1.6rem] px-[2rem] py-[1.65rem] focus:outline-none',
+        className
+      )}
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      maxLength={maxLength}
+      {...props}
+    />
+  );
+}
