b0f42b166bc6cbc42ccefe05d56c7e7adb3d3488
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-05 09:05:43 +0900

    feat: regions 전역 상태 추가

--- src/shared/querykey/regions.ts
+++ src/shared/querykey/regions.ts
@@ -2,14 +2,21 @@ import { DEPTHS } from '@pages/set-location/constant/location';
 
 export const REGION_QUERY_KEY = {
   ALL: ['regions'],
-  DEPTH1: () => [...REGION_QUERY_KEY.ALL, DEPTHS.ONE],
+  DEPTHS: () => [...REGION_QUERY_KEY.ALL, 'depth'],
+  DEPTH1: () => [
+    ...REGION_QUERY_KEY.ALL,
+    ...REGION_QUERY_KEY.DEPTHS(),
+    DEPTHS.ONE,
+  ],
   DEPTH2: (depth1Code: number) => [
     ...REGION_QUERY_KEY.ALL,
+    ...REGION_QUERY_KEY.DEPTHS(),
     DEPTHS.TWO,
     depth1Code,
   ],
   DEPTH3: (depth1Code: number, depth2Code: number) => [
     ...REGION_QUERY_KEY.ALL,
+    ...REGION_QUERY_KEY.DEPTHS(),
     DEPTHS.THREE,
     depth1Code,
     depth2Code,
