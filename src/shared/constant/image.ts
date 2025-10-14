397de84b9d1e74ff443eae076a943160d9bed302
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-14 11:02:01 +0900

    feat: image size util, constant 추가

--- src/shared/constant/image.ts
+++ src/shared/constant/image.ts
@@ -14,5 +14,8 @@ export const NOT_ALLOWED_FILE_TYPE =
 
 export const CANNOT_UPLOAD_FILE_MB = '5MB 이하의 파일만 업로드 가능합니다.';
 
+export const IMAGE_SIZE_MESSAGE =
+  '1280 x 960 (가로x세로) 사이즈 이미지를 업로드해주세요.';
+
 export const IMAGE_INFO_MESSAGE =
   '1280 x 960 (가로x세로) 사이즈 이상, 5MB 이하의 JPG/JPEG/PNG/SVG 파일만 첨부가 가능합니다.';
--- src/shared/utils/image.ts
+++ src/shared/utils/image.ts
@@ -1,4 +1,8 @@
-import { ALLOWED_FILE_EXTENSIONS, MAX_MB } from '@shared/constant/image';
+import {
+  ALLOWED_FILE_EXTENSIONS,
+  MAX_MB,
+  IMAGE_SIZE_RATIO,
+} from '@shared/constant/image';
 
 export const isAcceptableFile = (file: File) => {
   return file.type ? ALLOWED_FILE_EXTENSIONS.includes(file.type) : false;
@@ -6,4 +10,20 @@ export const isAcceptableFile = (file: File) => {
 
 export const isFileSizeValid = (file: File) => {
   return file.size <= MAX_MB * 1024 * 1024;
+};
+
+export const isImageSizeValid = (file: File) => {
+  const img = new Image();
+  const url = URL.createObjectURL(file);
+
+  img.onload = () => {
+    URL.revokeObjectURL(url);
+    return img.width >= IMAGE_SIZE_RATIO && img.height >= IMAGE_SIZE_RATIO;
+  };
+
+  img.onerror = () => {
+    URL.revokeObjectURL(url);
+    return false;
+  };
+  return false;
 };
