069b7e1e1d47ecdc66e661d02abfaf8a0f606e26
glyph8 <newdy0808@gmail.com>
2025-10-15 00:41:59 +0900

    fix: 빌드 오류 해결

--- src/pages/set-user-info/types/set-user-types.ts
+++ src/pages/set-user-info/types/set-user-types.ts
@@ -1,20 +1,7 @@
-import type { UseMutateFunction } from '@tanstack/react-query';
-import type {
-  BaseResponseVoid,
-  UpdateUserInfoRequest,
-  UserResponse,
-} from 'apis/data-contracts';
+import type { UserResponse } from 'apis/data-contracts';
 import React from 'react';
 
 export interface SetUserInfoItemProps {
   userInfo: UserResponse;
   setUserInfo: React.Dispatch<React.SetStateAction<UserResponse>>;
-  // setUserInfo: UseMutateFunction<
-  //   BaseResponseVoid,
-  //   Error,
-  //   UpdateUserInfoRequest,
-  //   unknown
-  // >;
-  // userInfo: UpdateUserInfoRequest;
-  // setUserInfo: React.Dispatch<React.SetStateAction<UpdateUserInfoRequest>>;
 }
