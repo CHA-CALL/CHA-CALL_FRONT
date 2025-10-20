47b962399ce9c0ce0676122007915325ce33bb40
junghyungo <pp90971658@gmail.com>
2025-10-20 23:30:13 +0900

    style: 사진 등록 ui 수정

--- src/pages/@owner/upload-food-truck/UploadFoodTruck.tsx
+++ src/pages/@owner/upload-food-truck/UploadFoodTruck.tsx
@@ -48,14 +48,17 @@ export default function UploadFoodTruck() {
       />
       <div className='flex w-full flex-col items-start justify-start gap-[1.2rem] p-[2rem]'>
         <div className='flex w-full flex-col items-start justify-start gap-[0.2rem]'>
-          <p className='title-sb-16 grayscale-900'>푸드트럭 사진 등록</p>
-          <p className='caption-m-11 grayscale-500'>
+          <p className='title-sb-16 text-grayscale-900'>
+            푸드트럭 사진 등록
+          </p>
+          <p className='caption-m-11 text-grayscale-500'>
             사진은 순서대로 등록해주세요. 첫 번째 사진이 썸네일로 보여요!
           </p>
         </div>
         <Information
-          iconId='ic_check'
-          text='사진은 16:9 비율로, 가능하면 고화질 이미지를 올려주세요!'
+          iconId='ic_chat_dot'
+          text='드래그하여 대표 이미지 설정 및 순서를 조정해보세요!'
+          className='w-full'
         />
       </div>
 
@@ -81,7 +84,7 @@ export default function UploadFoodTruck() {
           handleClickButton={handleSubmitImage}
           disabled={files.length === 0}
         >
-          등록하기
+          저장하기
         </Button>
       </footer>
     </>
