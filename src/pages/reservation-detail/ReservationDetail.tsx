677d5d3a08585733698d0599d9713bcf61456ac8
holdn2 <yoocy01@gmail.com>
2025-10-24 22:05:17 +0900

    feat: 가능한 todo 처리. 일정 관련 포맷팅 적용

--- src/pages/reservation-detail/ReservationDetail.tsx
+++ src/pages/reservation-detail/ReservationDetail.tsx
@@ -12,7 +12,7 @@ import { useReservationDetail } from '@pages/reservation-detail/hooks/use-reserv
 import ReservationDetailTopContent from '@pages/reservation-detail/components/ReservationDetailTopContent';
 
 export default function ReservationDetail() {
-  // TODO : 추후 툴팁 관련 커스텀 훅 만들어 관리
+  // TODO : 추후 툴팁 관련 커스텀 훅 만들어 관리. 언제 보여줄지 애매해서 논의 필요.
   const [isOpenTip, setIsOpenTip] = useState(true);
   const navigate = useNavigate();
 
--- src/pages/reservation-detail/components/ReservationDetailRow.tsx
+++ src/pages/reservation-detail/components/ReservationDetailRow.tsx
@@ -1,4 +1,5 @@
 import type { ReservationPartialInfo } from '@pages/reservation-detail/hooks/use-reservation-detail';
+import { formatDateTimeInfos } from '@shared/components/food-truck-card/utils/date-time-utils';
 
 interface ReservationDetailRowProps {
   title: string;
@@ -9,6 +10,28 @@ export default function ReservationDetailRow({
   title,
   infoList,
 }: ReservationDetailRowProps) {
+  const renderSchedule = (date: string, index: number) => {
+    const formattedSchedule = formatDateTimeInfos(date);
+    return (
+      <div
+        key={`${date}-${index}`}
+        className='flex flex-row items-center justify-end gap-[0.7rem] text-grayscale-700 body-m-13'
+      >
+        <div className='flex flex-row items-center gap-[0.2rem]'>
+          <span>{formattedSchedule[0]}</span>
+          <span>-</span>
+          <span>{formattedSchedule[1]}</span>
+        </div>
+        <div className='h-[1.05rem] w-[0.1rem] bg-grayscale-500' />
+        <div className='flex flex-row items-center gap-[0.2rem]'>
+          <span>{formattedSchedule[2]}</span>
+          <span>-</span>
+          <span>{formattedSchedule[3]}</span>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className='flex flex-col gap-[1.6rem] p-[0.5rem] title-sb-12'>
       <h2 className='text-grayscale-900 title-sb-14'>{title}</h2>
@@ -16,8 +39,9 @@ export default function ReservationDetailRow({
         <div key={label} className='flex justify-between'>
           <span className='whitespace-nowrap text-grayscale-500'>{label}</span>
           <span className='w-[40ch] whitespace-pre-line text-balance text-end text-grayscale-700'>
-            {/* TODO: reservation history api 관련 pr 머지 후 formatDateTimeInfos 함수 사용해서 포맷하기 */}
-            {Array.isArray(data) ? data.join('\n') : data}
+            {Array.isArray(data)
+              ? data.map((date, index) => renderSchedule(date, index))
+              : data}
           </span>
         </div>
       ))}
