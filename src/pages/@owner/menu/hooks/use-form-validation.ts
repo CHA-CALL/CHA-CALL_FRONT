113da747858000406961a9534685858d089b0486
junghyungo <pp90971658@gmail.com>
2025-10-30 00:06:53 +0900

    code review: 메뉴 스키마 수정

--- src/pages/@owner/menu/hooks/use-form-validation.ts
+++ src/pages/@owner/menu/hooks/use-form-validation.ts
@@ -1,62 +1,21 @@
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import {
-  MENU_LIMIT,
-  MENU_ERROR_MESSAGE,
-} from '@pages/@owner/menu/constant/menu';
-import {
-  CANNOT_UPLOAD_FILE_MB,
-  NOT_ALLOWED_FILE_TYPE,
-} from '@shared/constant/image';
-import { isAcceptableFile, isFileSizeValid } from '@shared/utils/image';
 import { formatPrice } from '@shared/utils/price-formatter';
+import {
+  MENU_NAME_VALIDATOR,
+  MENU_DESCRIPTION_VALIDATOR,
+  MENU_PRICE_VALIDATOR,
+  MENU_IMAGE_VALIDATOR,
+} from '@pages/@owner/menu/utils/menu-form-validator.schema';
 
-const menuSchema = z.object({
-  name: z
-    .string()
-    .min(
-      MENU_LIMIT.NAME_MIN_LENGTH,
-      MENU_ERROR_MESSAGE.NAME_MIN(MENU_LIMIT.NAME_MIN_LENGTH)
-    )
-    .max(
-      MENU_LIMIT.NAME_MAX_LENGTH,
-      MENU_ERROR_MESSAGE.NAME_MAX(MENU_LIMIT.NAME_MAX_LENGTH)
-    ),
-
-  description: z
-    .string()
-    .min(
-      MENU_LIMIT.DESCRIPTION_MIN_LENGTH,
-      MENU_ERROR_MESSAGE.DESCRIPTION_MIN(MENU_LIMIT.DESCRIPTION_MIN_LENGTH)
-    )
-    .max(
-      MENU_LIMIT.DESCRIPTION_MAX_LENGTH,
-      MENU_ERROR_MESSAGE.DESCRIPTION_MAX(MENU_LIMIT.DESCRIPTION_MAX_LENGTH)
-    ),
-
-  price: z
-    .string()
-    .refine(
-      val => val.replace(/,/g, '').length >= MENU_LIMIT.PRICE_MIN_LENGTH,
-      MENU_ERROR_MESSAGE.PRICE_MIN
-    )
-    .transform(val => formatPrice(val)),
-
-  imageUrl: z
-    .union([z.instanceof(File), z.undefined(), z.null()])
-    .refine(file => file instanceof File, {
-      message: MENU_ERROR_MESSAGE.IMAGE_MIN_COUNT(MENU_LIMIT.IMAGE_MIN_COUNT),
-    })
-    .refine(
-      file => (file ? isAcceptableFile(file) : true),
-      { message: NOT_ALLOWED_FILE_TYPE }
-    )
-    .refine(
-      file => (file ? isFileSizeValid(file) : true),
-      { message: CANNOT_UPLOAD_FILE_MB }
-    ),
-});
+const menuSchema = z
+  .object({
+    name: MENU_NAME_VALIDATOR,
+    description: MENU_DESCRIPTION_VALIDATOR,
+    price: MENU_PRICE_VALIDATOR,
+    imageUrl: MENU_IMAGE_VALIDATOR,
+  });
 
 export type MenuFormData = z.infer<typeof menuSchema>;
 
@@ -76,21 +35,13 @@ export const useFormValidation = (initialData?: Partial<MenuFormData>) => {
   const {
     setValue,
     formState: { isValid, errors },
-    // watch,
   } = methods;
-
-  // const formData = watch();
 
   const updateName = (name: string) => {
     setValue('name', name, { shouldValidate: true });
   };
 
   const updateDescription = (description: string) => {
-    // const truncatedDescription = description.slice(
-    //   0,
-    //   MENU_LIMIT.DESCRIPTION_MAX_LENGTH
-    // );
-    // setValue('description', truncatedDescription, { shouldValidate: true });
     setValue('description', description, { shouldValidate: true });
   };
 
@@ -111,7 +62,6 @@ export const useFormValidation = (initialData?: Partial<MenuFormData>) => {
 
   return {
     methods,
-    // formData,
     errors: Errors,
     isValid,
     updateName,
---
+++ src/pages/@owner/menu/utils/menu-form-validator.schema.ts
@@ -0,0 +1,55 @@
+import { z } from 'zod';
+import {
+  MENU_LIMIT,
+  MENU_ERROR_MESSAGE,
+} from '@pages/@owner/menu/constant/menu';
+import {
+  CANNOT_UPLOAD_FILE_MB,
+  NOT_ALLOWED_FILE_TYPE,
+} from '@shared/constant/image';
+import { isAcceptableFile, isFileSizeValid } from '@shared/utils/image';
+import { formatPrice } from '@shared/utils/price-formatter';
+
+export const MENU_NAME_VALIDATOR = z
+  .string()
+  .min(
+    MENU_LIMIT.NAME_MIN_LENGTH,
+    MENU_ERROR_MESSAGE.NAME_MIN(MENU_LIMIT.NAME_MIN_LENGTH)
+  )
+  .max(
+    MENU_LIMIT.NAME_MAX_LENGTH,
+    MENU_ERROR_MESSAGE.NAME_MAX(MENU_LIMIT.NAME_MAX_LENGTH)
+  );
+
+export const MENU_DESCRIPTION_VALIDATOR = z
+  .string()
+  .min(
+    MENU_LIMIT.DESCRIPTION_MIN_LENGTH,
+    MENU_ERROR_MESSAGE.DESCRIPTION_MIN(MENU_LIMIT.DESCRIPTION_MIN_LENGTH)
+  )
+  .max(
+    MENU_LIMIT.DESCRIPTION_MAX_LENGTH,
+    MENU_ERROR_MESSAGE.DESCRIPTION_MAX(MENU_LIMIT.DESCRIPTION_MAX_LENGTH)
+  );
+
+export const MENU_PRICE_VALIDATOR = z
+  .string()
+  .refine(
+    val => val.replace(/,/g, '').length >= MENU_LIMIT.PRICE_MIN_LENGTH,
+    MENU_ERROR_MESSAGE.PRICE_MIN
+  )
+  .transform(val => formatPrice(val));
+
+export const MENU_IMAGE_VALIDATOR = z
+  .union([z.instanceof(File), z.undefined(), z.null()])
+  .refine(file => file instanceof File, {
+    message: MENU_ERROR_MESSAGE.IMAGE_MIN_COUNT(MENU_LIMIT.IMAGE_MIN_COUNT),
+  })
+  .refine(
+    file => (file ? isAcceptableFile(file) : true),
+    { message: NOT_ALLOWED_FILE_TYPE }
+  )
+  .refine(
+    file => (file ? isFileSizeValid(file) : true),
+    { message: CANNOT_UPLOAD_FILE_MB }
+  );
