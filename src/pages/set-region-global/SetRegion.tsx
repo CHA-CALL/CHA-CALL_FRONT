14908a314cd9b59e3f025d50ca92083d1bda9cf1
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-10-31 10:11:31 +0900

    refactor: 위치설정 global

---
+++ src/pages/set-region-global/SetRegion.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import useRegion from '@pages/set-region-global/hooks/use-region';
+
+import { Icon } from '@icon/Icon';
+import Navigation from '@layout/navigation/Navigation';
+import Region from '@shared/components/region/Region';
+
+export default function SetRegion() {
+  const { initialRegions, handleConfirmRegion } = useRegion();
+  const navigate = useNavigate();
+  const handleClickBack = () => navigate(-1);
+
+  return (
+    <>
+      <Navigation
+        text='위치설정'
+        leftIcon={<Icon name='ic_back' />}
+        handleLeftClick={handleClickBack}
+      />
+      <Region
+        initialRegions={Array.from(initialRegions)}
+        handleConfirmRegion={handleConfirmRegion}
+      />
+    </>
+  );
+}
