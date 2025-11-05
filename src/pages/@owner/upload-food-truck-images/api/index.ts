f1bf3049c68b09a0f9fb48eda5e9b73199606739
junghyungo <pp90971658@gmail.com>
2025-11-05 17:16:27 +0900

    docs: 불필요한 상수 제거 등

--- src/pages/@owner/upload-food-truck-images/api/index.ts
+++ src/pages/@owner/upload-food-truck-images/api/index.ts
@@ -24,11 +24,7 @@ export const getPresignedUrl = async (fileExtensions: string[]): Promise<ImageIn
 };
 
 export const uploadImage = async (presignedUrl: string, file: File) => {
-  // TODO: 삭제 및 presignedUrl로 수정 (이미지 업로드 테스트용)
-  const url = new URL(presignedUrl);
-  const proxiedUrl = `/s3-proxy${url.pathname}${url.search}`;
-
-  const response = await fetch(proxiedUrl, {
+  const response = await fetch(presignedUrl, {
     method: 'PUT',
     headers: {
       'Content-Type': file.type,
