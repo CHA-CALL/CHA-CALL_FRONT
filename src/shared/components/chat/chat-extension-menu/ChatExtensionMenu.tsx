066154e21e2a65cf5508cb211ec64dc4f38ec501
glyph8 <newdy0808@gmail.com>
2025-11-07 11:32:49 +0900

    feat: 메세지 입력 textarea 작성

--- src/shared/components/chat/chat-extension-menu/ChatExtensionMenu.tsx
+++ src/shared/components/chat/chat-extension-menu/ChatExtensionMenu.tsx
@@ -1,21 +1,50 @@
-import { useState } from 'react';
-import ExtensionMenuItem from './components/ExtensionMenuItem';
-import { Icon } from '@shared/components/icon/Icon';
+import { useRef, useState, type FormEvent } from 'react';
+import ExtensionMenuItem from '@components/chat/chat-extension-menu/components/ExtensionMenuItem';
+import { Icon } from '@components/icon/Icon';
+import useAutosizeTextarea from '@components/chat/chat-extension-menu/hooks/use-autosize-textarea';
+import { cn } from '@utils/cn';
 
 export default function ChatExtensionMenu() {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [message, setMessage] = useState('');
   const [active, setActive] = useState(false);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  useAutosizeTextarea(textAreaRef.current, message);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!message.trim()) return;
+    setMessage('');
+  };
+
+  const handleExtensionClick = () => {
+    textAreaRef.current?.blur();
+    setIsOpenMenu(prev => !prev);
+  };
+
+  const textAreaBaseClasses =
+    ' bg-grayscale-100 body-m-14 w-full resize-none overflow-hidden rounded-[5rem] border-none px-[1.8rem] py-[0.8rem] ';
+  const textAreaInputclasses =
+    'caret-grayscale-900 placeholder:text-grayscale-500 focus:outline-none';
+
   return (
     <div>
-      <div className='flex gap-[0.5rem] px-[1.1rem] py-[0.8rem]'>
-        <button className='bg-grayscale-900 flex items-center justify-center rounded-full p-[0.9rem]'>
+      <div className='flex items-center gap-[0.5rem] px-[1.1rem] py-[0.8rem]'>
+        <button
+          type='button'
+          className='bg-grayscale-900 flex h-[2.2rem] w-[2.2rem] items-center justify-center rounded-full p-[0.5rem]'
+          onClick={handleExtensionClick}
+        >
           <Icon name={'ic_plus'} className='h-[0.8rem] w-[0.8rem] text-white' />
         </button>
-        <form className='flex w-full'>
-          <input
-            type='text'
+        <form onSubmit={handleSubmit} className='flex w-full'>
+          <textarea
+            ref={textAreaRef}
+            value={message}
+            onChange={e => setMessage(e.target.value)}
             placeholder='메세지를 입력하세요.'
-            className='bg-grayscale-100 w-full rounded-[5rem] px-[1.8rem] py-[0.8rem]'
+            rows={1}
+            className={cn(textAreaBaseClasses, textAreaInputclasses)}
           />
           <button
             type='submit'
@@ -25,77 +54,79 @@ export default function ChatExtensionMenu() {
           </button>
         </form>
       </div>
-      <div className='border-1 flex flex-col items-center gap-[1.6rem] px-[4rem] py-[2rem]'>
-        <nav className='flex gap-[2.4rem]'>
-          <ExtensionMenuItem
-            title={'카메라'}
-            icon={<Icon name={'ic_camera'} />}
-            isDisabled={active}
-            handleClick={() => {
-              setActive(prev => !prev);
-            }}
-          />
-          <ExtensionMenuItem
-            title={'카메라'}
-            icon={<Icon name={'ic_camera'} />}
-            isDisabled={active}
-            handleClick={() => {
-              setActive(prev => !prev);
-            }}
-          />
-          <ExtensionMenuItem
-            title={'카메라'}
-            icon={<Icon name={'ic_camera'} />}
-            isDisabled={active}
-            handleClick={() => {
-              setActive(prev => !prev);
-            }}
-          />
-          <ExtensionMenuItem
-            title={'카메라'}
-            icon={<Icon name={'ic_camera'} />}
-            isDisabled={active}
-            handleClick={() => {
-              setActive(prev => !prev);
-            }}
-          />
-        </nav>
+      {isOpenMenu && (
+        <div className='flex flex-col items-center gap-[1.6rem] px-[4rem] py-[2rem]'>
+          <nav className='flex gap-[2.4rem]'>
+            <ExtensionMenuItem
+              title={'카메라'}
+              icon={<Icon name={'ic_camera'} />}
+              isDisabled={active}
+              handleClick={() => {
+                setActive(prev => !prev);
+              }}
+            />
+            <ExtensionMenuItem
+              title={'카메라'}
+              icon={<Icon name={'ic_camera'} />}
+              isDisabled={active}
+              handleClick={() => {
+                setActive(prev => !prev);
+              }}
+            />
+            <ExtensionMenuItem
+              title={'카메라'}
+              icon={<Icon name={'ic_camera'} />}
+              isDisabled={active}
+              handleClick={() => {
+                setActive(prev => !prev);
+              }}
+            />
+            <ExtensionMenuItem
+              title={'카메라'}
+              icon={<Icon name={'ic_camera'} />}
+              isDisabled={active}
+              handleClick={() => {
+                setActive(prev => !prev);
+              }}
+            />
+          </nav>
 
-        <nav className='flex gap-[2.4rem]'>
-          <ExtensionMenuItem
-            title={'카메라'}
-            icon={<Icon name={'ic_camera'} />}
-            isDisabled={active}
-            handleClick={() => {
-              setActive(prev => !prev);
-            }}
-          />
-          <ExtensionMenuItem
-            title={'카메라'}
-            icon={<Icon name={'ic_camera'} />}
-            isDisabled={active}
-            handleClick={() => {
-              setActive(prev => !prev);
-            }}
-          />
-          <ExtensionMenuItem
-            title={'카메라'}
-            icon={<Icon name={'ic_camera'} />}
-            isDisabled={active}
-            handleClick={() => {
-              setActive(prev => !prev);
-            }}
-          />
-          <ExtensionMenuItem
-            title={'카메라'}
-            icon={<Icon name={'ic_camera'} />}
-            isDisabled={active}
-            handleClick={() => {
-              setActive(prev => !prev);
-            }}
-          />
-        </nav>
-      </div>
+          <nav className='flex gap-[2.4rem]'>
+            <ExtensionMenuItem
+              title={'카메라'}
+              icon={<Icon name={'ic_camera'} />}
+              isDisabled={active}
+              handleClick={() => {
+                setActive(prev => !prev);
+              }}
+            />
+            <ExtensionMenuItem
+              title={'카메라'}
+              icon={<Icon name={'ic_camera'} />}
+              isDisabled={active}
+              handleClick={() => {
+                setActive(prev => !prev);
+              }}
+            />
+            <ExtensionMenuItem
+              title={'카메라'}
+              icon={<Icon name={'ic_camera'} />}
+              isDisabled={active}
+              handleClick={() => {
+                setActive(prev => !prev);
+              }}
+            />
+            <ExtensionMenuItem
+              title={'카메라'}
+              icon={<Icon name={'ic_camera'} />}
+              isDisabled={active}
+              handleClick={() => {
+                setActive(prev => !prev);
+              }}
+            />
+          </nav>
+        </div>
+      )}
     </div>
   );
 }
