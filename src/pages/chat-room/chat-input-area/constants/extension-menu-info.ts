bfdb5a60f1fb172666b53d4d67f6e3117429c234
glyph8 <newdy0808@gmail.com>
2025-11-10 14:37:19 +0900

    feat: 타입 유지보수 강화

--- src/pages/chat-room/chat-input-area/constants/extension-menu-info.ts
+++ src/pages/chat-room/chat-input-area/constants/extension-menu-info.ts
@@ -1,4 +1,4 @@
-import type { Role } from '@constant/role';
+import { ROLE, type Role } from '@constant/role';
 
 export type MenuKey =
   | 'gallery'
@@ -11,16 +11,7 @@ export type MenuKey =
   | 'write_paper'
   | 'edit_paper';
 
-export type MenuIconId =
-  | 'ic_camera'
-  | 'ic_gallery'
-  | 'ic_ment'
-  | 'ic_cash'
-  | 'ic_write_paper'
-  | 'ic_edit_paper'
-  | 'ic_view_paper'
-  | 'ic_cancel'
-  | 'ic_download_paper';
+export type MenuIconId = `ic_${MenuKey}`;
 
 export interface MenuItemConfig {
   key: MenuKey;
@@ -35,56 +26,56 @@ export const ALL_MENU_ITEMS: MenuItemConfig[] = [
     key: 'gallery',
     title: '앨범',
     iconId: 'ic_gallery',
-    roles: ['client', 'provider'],
+    roles: [ROLE.CLIENT, ROLE.PROVIDER],
   },
   {
     key: 'camera',
     title: '카메라',
     iconId: 'ic_camera',
-    roles: ['client', 'provider'],
+    roles: [ROLE.CLIENT, ROLE.PROVIDER],
   },
   {
     key: 'view_paper',
     title: '견적서 보기',
     iconId: 'ic_view_paper',
-    roles: ['client', 'provider'],
+    roles: [ROLE.CLIENT, ROLE.PROVIDER],
   },
   {
     key: 'cancel',
     title: '예약 취소 신청',
     iconId: 'ic_cancel',
-    roles: ['client', 'provider'],
+    roles: [ROLE.CLIENT, ROLE.PROVIDER],
   },
   // Client 전용
   {
     key: 'download_paper',
     title: '견적서 다운',
     iconId: 'ic_download_paper',
-    roles: ['client'],
+    roles: [ROLE.CLIENT],
   },
   // Provider 전용
   {
     key: 'ment',
     title: '자주쓰는 문구',
     iconId: 'ic_ment',
-    roles: ['provider'],
+    roles: [ROLE.PROVIDER],
   },
   {
     key: 'cash',
     title: '계좌번호',
     iconId: 'ic_cash',
-    roles: ['provider'],
+    roles: [ROLE.PROVIDER],
   },
   {
     key: 'write_paper',
     title: '견적서 작성',
     iconId: 'ic_write_paper',
-    roles: ['provider'],
+    roles: [ROLE.PROVIDER],
   },
   {
     key: 'edit_paper',
     title: '견적서 수정',
     iconId: 'ic_edit_paper',
-    roles: ['provider'],
+    roles: [ROLE.PROVIDER],
   },
 ];
