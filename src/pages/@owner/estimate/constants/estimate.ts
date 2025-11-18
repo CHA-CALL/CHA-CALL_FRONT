2e7341b17b74ddbda16b84010083df73d7f0640e
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-11-18 10:15:33 +0900

    feat: 예약 견적서 스크마 작성

---
+++ src/pages/@owner/estimate/constants/estimate.ts
@@ -0,0 +1,44 @@
+export const ESTIMATE_MAX_LENGTH = {
+  location: {
+    min: 1,
+    max: 20,
+  },
+  menu: {
+    min: 1,
+    max: 100,
+  },
+  etc: {
+    max: 200,
+  },
+  availableDates: {
+    min: 1,
+    max: 4,
+  },
+} as const;
+
+export const ESTIMATE_ERROR_MESSAGE = {
+  location: {
+    min: '시/군/구를 입력해주세요.',
+    max: `시/군/구는 ${ESTIMATE_MAX_LENGTH.location.max}자 이하로 입력해주세요.`,
+  },
+  menu: {
+    required: '음식을 입력해주세요.',
+    max: `음식은 ${ESTIMATE_MAX_LENGTH.menu.max}자 이하로 입력해주세요.`,
+  },
+  price: {
+    required: '금액을 입력해주세요.',
+  },
+  availableDates: {
+    min: `가능한 일정대는 ${ESTIMATE_MAX_LENGTH.availableDates.min}개 이상 선택해주세요.`,
+    max: `가능한 일정대는 ${ESTIMATE_MAX_LENGTH.availableDates.max}개 이하로 선택해주세요.`,
+    invalid: '일정이 겹칩니다. 다른 일정을 선택해주세요.',
+  },
+  activeTime: {
+    start: `운영 시작 시간을 선택해주세요.`,
+    end: `운영 종료 시간을 선택해주세요.`,
+    invalid: `운영 시작 시간은 운영 종료 시간보다 앞에 있어야합니다.`,
+  },
+  etc: {
+    max: `푸드트럭 운영 정보는 ${ESTIMATE_MAX_LENGTH.etc.max}자 이하로 입력해주세요.`,
+  },
+} as const;
