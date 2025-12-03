52d88b1cb82d1dd57282ab3c5d67a30f46d9f7cd
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-12-03 16:55:52 +0900

    feat: form row layout component

---
+++ src/shared/components/layout/form/FormFieldLayout.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { cn } from '@utils/cn';
+
+interface FormFieldLayoutProps {
+  isRequired: boolean;
+  title: string;
+  subTitle?: string;
+  description?: string;
+  children: React.ReactNode;
+  borderBottom?: boolean;
+  rightComponent?: React.ReactNode;
+  className?: string;
+}
+
+export default function FormFieldLayout({
+  isRequired,
+  title,
+  subTitle,
+  description,
+  children,
+  borderBottom = false,
+  rightComponent,
+  className,
+}: FormFieldLayoutProps) {
+  return (
+    <div
+      className={cn(
+        'flex flex-col justify-start gap-[1.2rem] py-[2.6rem]',
+        borderBottom && 'border-b border-grayscale-100',
+        className
+      )}
+    >
+      <div>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center gap-[0.6rem] title-sb-14'>
+            <span className='text-grayscale-900'>{title}</span>
+            {subTitle && (
+              <span className='text-grayscale-500 caption-m-12'>
+                {subTitle}
+              </span>
+            )}
+
+            {isRequired && <span className='text-primary-700'>*</span>}
+          </div>
+          {rightComponent && rightComponent}
+        </div>
+        {description && (
+          <span className='text-grayscale-500 caption-m-12'>{description}</span>
+        )}
+      </div>
+      {children}
+    </div>
+  );
+}
