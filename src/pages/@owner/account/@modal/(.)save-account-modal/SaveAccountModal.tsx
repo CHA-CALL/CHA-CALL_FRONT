edf8d1fca3cdcd5c8e92baf24609057ac2f9efe2
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-12-03 16:57:29 +0900

    refactor: modal index 파일 생성 및 불필요 코드 삭제

--- src/pages/@owner/account/@modal/(.)save-account-modal/SaveAccountModal.tsx
+++ src/pages/@owner/account/@modal/(.)save-account-modal/SaveAccountModal.tsx
@@ -1,5 +1,5 @@
 import Overlay from '@layout/overlay/Overlay';
-import { type AccountFormData } from '@pages/@owner/account/hooks/use-account';
+import { type AccountFormData } from '@pages/@owner/account/schema/account.schema';
 
 import Button from '@ui/button/Button';
 
@@ -21,24 +21,24 @@ export default function SaveAccountModal({
   return (
     <Overlay isOpen={isOpen} handleClose={handleClose}>
       <div className='flex min-w-[27.4rem] flex-col gap-[1rem] rounded-[1.6rem] bg-white px-[2rem] pb-[2rem] pt-[2.4rem]'>
-        <p className='heading-sb-18 text-black'>저장정보 확인</p>
-        <div className='bg-grayscale-100 h-[0.1rem] w-full' />
+        <p className='text-black heading-sb-18'>저장정보 확인</p>
+        <div className='h-[0.1rem] w-full bg-grayscale-100' />
         <div className='flex flex-col gap-[1rem]'>
           <div className='flex items-center justify-between'>
-            <p className='title-sb-12 text-grayscale-500'>은행</p>
-            <p className='caption-m-12 text-grayscale-900'>
+            <p className='text-grayscale-500 title-sb-12'>은행</p>
+            <p className='text-grayscale-900 caption-m-12'>
               {formData.bankName}
             </p>
           </div>
           <div className='flex items-center justify-between'>
-            <p className='title-sb-12 text-grayscale-500'>예금주</p>
-            <p className='caption-m-12 text-grayscale-900'>
+            <p className='text-grayscale-500 title-sb-12'>예금주</p>
+            <p className='text-grayscale-900 caption-m-12'>
               {formData.accountHolderName}
             </p>
           </div>
           <div className='flex items-center justify-between'>
-            <p className='title-sb-12 text-grayscale-500'>계좌번호</p>
-            <p className='caption-m-12 text-grayscale-900'>
+            <p className='text-grayscale-500 title-sb-12'>계좌번호</p>
+            <p className='text-grayscale-900 caption-m-12'>
               {formData.accountNumber}
             </p>
           </div>
