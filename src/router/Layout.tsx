c4dc704e34da3cbb93c5f78c5957c279b64e8a4e
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-12-13 12:27:23 +0900

    feat: suspense 적용

--- src/router/Layout.tsx
+++ src/router/Layout.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import Loading from '@layout/loading/Loading';
 
 export default function Layout() {
   return (
     <main className='mx-auto min-h-dvh max-w-[60rem] pt-[4.8rem]'>
-      <Outlet />
+      <Suspense fallback={<Loading />}>
+        <Outlet />
+      </Suspense>
     </main>
   );
 }
