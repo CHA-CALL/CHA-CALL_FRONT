b9a043a64040f0c085b9e642c15d826f3adb2ddd
holdn2 <yoocy01@gmail.com>
2025-12-21 12:22:35 +0900

    code review: 예약 상태 관련 에러 처리

--- src/pages/chat-room/chat-input-area/hooks/use-reservation-status.ts
+++ src/pages/chat-room/chat-input-area/hooks/use-reservation-status.ts
@@ -1,16 +1,29 @@
+import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import type { ReservationStatusResponse } from 'apis/data-contracts';
 
-import { getReservationStatus } from '@pages/chat-room/api';
+import useToast from '@hooks/use-toast';
+import { ROUTES } from '@router/constant/routes';
 import { CHAT_QUERY_KEY } from '@shared/querykey/chat';
+import { getReservationStatus } from '@pages/chat-room/api';
 
 export default function useReservationStatus(reservationId: number | null) {
-  const { data: reservationStatus, isPending: isReservationStatusPending } =
-    useQuery<ReservationStatusResponse | undefined>({
-      queryKey: CHAT_QUERY_KEY.RESERVATION_STATUS(reservationId),
-      queryFn: () => getReservationStatus(reservationId),
-      enabled: reservationId !== null,
-    });
+  const navigate = useNavigate();
+  const toast = useToast();
+  const {
+    data: reservationStatus,
+    isPending: isReservationStatusPending,
+    isError,
+  } = useQuery<ReservationStatusResponse | undefined>({
+    queryKey: CHAT_QUERY_KEY.RESERVATION_STATUS(reservationId),
+    queryFn: () => getReservationStatus(reservationId),
+    enabled: reservationId !== null,
+  });
+
+  if (isError) {
+    toast.error('예약 상태를 불러오는데 실패했습니다.');
+    navigate(ROUTES.CHAT_LIST);
+  }
 
   return {
     reservationStatus,
